e47920ec8409bb3c74b60e7c10c3bebd94191a70
Daler <dsarifkulo021@gmail.com>
2022-05-25 19:26:46 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,106 @@
+// let book = "avatar"
+
+// book = "harry potter"
+
+// console.log(book);
+
+
+// let book = "avatar" // string // 
+// let age = 22 // number
+// let isMarried = true // false // boolean
+
+// let arr = [] // obj
+// let obj = {} // obj
+
+// let err = NaN // not a number // number
+// let err2 = undefined // undefined
+// let err3 = null // object
+
+// console.log(
+//     typeof(age)
+// );
+
+// +
+// -
+// *
+// /
+
+// ** возведение в степень
+// % деление с остатком
+
+// console.log(
+//     7 / 2
+// );
+
+
+
+
+// 
+
+
+// name = name.toUpperCase() // camel case
+// name = name.toLowerCase() // camel case
+// name.slice(name.search("world"),11)
+// name.search("world")
+// name.charAt(6)
+// name[6]
+// name.split(" ")
+// name.trim()
+// name.includes("amet")
+// name.indexOf("dolor") 
+// name.replace("dollor", "sum")
+
+// let name = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente, id?"
+
+// console.log(
+// );
+
+// let str = "helloworld" // "Hello world"
+// str[0].toUpperCase() + str.slice(1,5) + " " + str.slice(5,10)
+// let rnd = Math.random().toString().slice(2,3)
+// let rnd = Math.random() * 100
+
+
+// console.log(
+//     rnd
+// );
+
+
+
+// методы работ с цифрами
+
+// let int = 10.49
+// let rnd = Math.random()
+
+// let a = 22 // string
+// let b = 32 // string
+
+// console.log(
+    // Math.floor(int) // to down
+    // Math.ceil(int) // to up
+    // Math.round(int) // logical
+    // Math.round( rnd * 10 )
+
+    // parseInt(a) // переводит в число и делает ее целым
+    // parseFloat(a) // переводит в число но не делает ее целым
+    // +a // переводит в число но не делает ее целым
+
+    // Math.max(a, b)
+    // Math.min(a, b)
+
+// );
+
+
+// let int = 1
+
+// increment
+// int = int + 1
+// int += 1
+// int++
+
+// // decrement
+// int = int - 1
+// int -= 1
+// int--
+
+// console.log(int);
